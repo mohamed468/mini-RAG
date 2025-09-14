9249d1d04c89d2a5a788e724c3e0c2d498e4ed8a
mohamed468 <muhammad.abdelfattah456@gmail.com>
2025-09-14 15:46:45 +0300

    return uploaded file id

--- src/routes/data.py
+++ src/routes/data.py
@@ -35,7 +35,7 @@ async def upload_data(project_id: str,
         )
     
     project_dir_path= ProjectController().get_project_path(project_id=project_id)
-    file_path= data_controller.generate_unique_filename(
+    file_path, file_id= data_controller.generate_unique_filepath(
         orig_file_name= file.filename,
         project_id= project_id
     )
@@ -49,14 +49,16 @@ async def upload_data(project_id: str,
         return JSONResponse(
             status_code=status.HTTP_400_BAD_REQUEST,
             content= {
-                "signal" : ResponseSignal.FILE_UPLOAD_FAILEd.value
+                "signal" : ResponseSignal.FILE_UPLOAD_FAILEd.value,
+                
             }
         )
 
 
     return JSONResponse(
         content= {
-            "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value
+            "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+            "file_id" : file_id
         }
     )            
 
